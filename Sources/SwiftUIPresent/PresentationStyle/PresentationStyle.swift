61a9f5bad819f0d7b116d30cfef0b248c05ce49b
Lumisilk <ribilynn@gmail.com>
2023-10-13 18:09:21 +0900

    Add docs for PresentationStyle

--- Sources/SwiftUIPresent/PresentationStyle/PresentationStyle.swift
+++ Sources/SwiftUIPresent/PresentationStyle/PresentationStyle.swift
@@ -8,18 +8,44 @@
 import UIKit
 import SwiftUI
 
+/// Properties and details relevant to presentation.
 public struct PresentationConfiguration {
-    public let id: AnyHashable
+    let id: AnyHashable
+    
+    /// A binding to a Boolean value that determines whether to present the view that you create in the modifier’s content closure.
     public let isPresented: Binding<Bool>
+    
+    /// The original SwiftUI view to which the presentation modifier is attached.
     public let anchorView: UIView
+    
+    /// The context for the current state-updating cycle, derived from UIViewRepresentable's context.
     public let transaction: Transaction
+    
+    /// The actual content to display, as provided within the presentation modifier.
     public let content: AnyView
 }
 
+/// A protocol defining how to present the provided content.
 public protocol PresentationStyle {
+    /// The type of view controller to present.
     associatedtype HostingControllerType: UIViewController
     
+    /// Creates the UIKit view controller and initializes its content and presentation style.
+    ///
+    /// You should create a new view controller, typically a subclass of UIHostingController<AnyView>.
+    /// Then, based on the information provided in the configuration, adjust its modalPresentationStyle and/or modalTransitionStyle.
+    /// Finally, pass the view contents from the configuration to the view controller for display.
+    ///
+    /// - Parameter configuration: Properties and details relevant to presentation.
+    /// - Returns: Your UIKit view configured with the provided information.
     func makeHostingController(_ configuration: PresentationConfiguration) -> HostingControllerType
+    
+    /// Updates the state of the specified view controller with new information.
+    ///
+    /// This method is called when the View Controller remains visible on the screen, but either the style or the present content has been updated. You should reflect the updated information in the view controller.
+    /// - Parameters:
+    ///   - hostingController: Your custom view controller.
+    ///   - configuration: Properties and details relevant to presentation.
     func update(_ hostingController: HostingControllerType, configuration: PresentationConfiguration)
 }
 
